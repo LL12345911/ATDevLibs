e11c32e15ac1fd656d917875f9ab770950624a18
Mars <sky12345911@163.com>
2022-08-26 16:30:41 +0800

    no message

--- ATDevLibs/UIKitLib/TabBar/MarsTabBar.m
+++ ATDevLibs/UIKitLib/TabBar/MarsTabBar.m
@@ -46,17 +46,18 @@
 
 - (void) safeAreaInsetsDidChange{
     [super safeAreaInsetsDidChange];
-    if(self.oldSafeAreaInsets.left != self.safeAreaInsets.left ||
-       self.oldSafeAreaInsets.right != self.safeAreaInsets.right ||
-       self.oldSafeAreaInsets.top != self.safeAreaInsets.top ||
-       self.oldSafeAreaInsets.bottom != self.safeAreaInsets.bottom)
-    {
-        self.oldSafeAreaInsets = self.safeAreaInsets;
-        [self invalidateIntrinsicContentSize];
-        [self.superview setNeedsLayout];
-        //[self.superview layoutSubviews];
+    if(@available(iOS 11.0, *)){
+        if(self.oldSafeAreaInsets.left != self.safeAreaInsets.left ||
+           self.oldSafeAreaInsets.right != self.safeAreaInsets.right ||
+           self.oldSafeAreaInsets.top != self.safeAreaInsets.top ||
+           self.oldSafeAreaInsets.bottom != self.safeAreaInsets.bottom)
+        {
+            self.oldSafeAreaInsets = self.safeAreaInsets;
+            [self invalidateIntrinsicContentSize];
+            [self.superview setNeedsLayout];
+            //[self.superview layoutSubviews];
+        }
     }
-    
 }
 
 - (CGSize) sizeThatFits:(CGSize) size{
